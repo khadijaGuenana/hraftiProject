43716213d31e01f780177ec93e6681456eb5e9dc
KHDIJA <khadija@email.com>
2022-11-02 12:11:01 +0100

    helper modified

--- app/src/main/java/com/example/hraftiproject/Helper.java
+++ app/src/main/java/com/example/hraftiproject/Helper.java
@@ -13,6 +13,8 @@ public class Helper extends SQLiteOpenHelper {
     private static final String TABLE_NAME = "professionnel";
     private static final String ID_COL = "id";
     private static final String Nom_COL = "nomComplet";
+    private static final String EMAIL_COL = "email";
+    private static final String PASSWORD_COL = "mdps";
     private static final String Metier_COL = "metier";
     private static final String numTel_COL = "numTel";
     private static final String ville_COL = "ville";
@@ -33,21 +35,25 @@ public class Helper extends SQLiteOpenHelper {
         String query = "CREATE TABLE " + TABLE_NAME + " ("
                 + ID_COL + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + Nom_COL + " TEXT,"
+                +EMAIL_COL+ "TEXT,"
+                +PASSWORD_COL+"TEXT"
                 + Metier_COL + " TEXT,"
                 + numTel_COL + " INTEGER,"
                 + ville_COL + " TEXT,"
                 + description_COL + " TEXT)";
-        //IMAGE
+        //IMAGE IN DB
 
        // String query="CREATE TABLE professionnel(_id INTEGER PRIMARY KEY,nomComplet TEXT ,metier TEXT ,numTel INTEGER ,ville TEXT ,description Text)";
 
         sqLiteDatabase.execSQL(query);
 
     }
-    public void addNewProfessionnel(String nomComplet,String metier ,int numTel ,String ville ,String description){
+    public void addNewProfessionnel(String nomComplet,String email, String password ,String metier ,int numTel ,String ville ,String description){
         SQLiteDatabase db=this.getWritableDatabase();
         ContentValues values=new ContentValues();
         values.put(Nom_COL, nomComplet);
+        values.put(EMAIL_COL, email);
+        values.put(PASSWORD_COL, password);
         values.put(Metier_COL , metier);
         values.put(numTel_COL , numTel);
         values.put(ville_COL, ville);
